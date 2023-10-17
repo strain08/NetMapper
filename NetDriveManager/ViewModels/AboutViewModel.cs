29d82e73e8f96dff7bf36729aa9f6de2f6061b8f
filip <filip.muresan@gmail.com>
2023-10-17 19:05:48 +0300

    about

--- NetDriveManager/ViewModels/AboutViewModel.cs
+++ NetDriveManager/ViewModels/AboutViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,16 @@ namespace NetMapper.ViewModels
 {
     public class AboutViewModel:ViewModelBase
     {
+        public AboutViewModel()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string fileVersion = FileVersionInfo.GetVersionInfo(assembly.Location).ProductMajorPart.ToString();
+            string versionMajor = fvi.FileMajorPart.ToString();
+            string versionMinor = fvi.FileMinorPart.ToString();
+            string versionFix = fvi.FileBuildPart.ToString();
+            Debug.Print(versionMajor + " " + versionMinor + " " + versionFix);
+        }
         public void HandleLinkClicked() 
         {
             // UriBuilder builder = new UriBuilder{ Path = @"https:\\github.com" };
